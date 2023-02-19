44261dd1ef060b2a47ad800c91931643232f3859
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-19 22:50:59 +0100

    v1

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,10 @@
-console.log('Hello World');
+const tween = KUTE.fromTo(
+    '#blob1',
+    { path: '#blob1' },
+    { path: '#blob2' },
+    { repeat: 999, yoyo: true, duration: 2000 }
 
+)
 
-
-
-
+tween.start()
 
