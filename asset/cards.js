08a2167ee965e021243d15e8d4fa966cc2e85137
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-23 02:07:09 +0100

    ready to go

--- asset/cards.js
+++ asset/cards.js
@@ -1,14 +1,19 @@
 const page1 = document.querySelector(".page1");
 const page2 = document.querySelector(".page2");
 const page3 = document.querySelector(".page3");
+const homeLink = document.querySelector(".home_link");
 
 function toggleActiveClass(element) {
-  element.addEventListener("click", function () {
+  element.addEventListener("click", function (e) {
     element.classList.toggle("active");
+
+    if (e.target.classList[0] === "page1") {
+      homeLink.classList.toggle("show");
+    }
   });
 }
 
 toggleActiveClass(page1);
 toggleActiveClass(page2);
 toggleActiveClass(page3);
-
+toggleActiveClass(homeLink);
