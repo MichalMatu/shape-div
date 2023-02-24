2a8992d6329cad54fdffa6e1ae4d803d9fa9fb86
Michal Matuszewski <michal.matuszewski1984@gmail.com>
2023-02-24 02:28:34 +0100

    three started

---
+++ asset/three_js.js
@@ -0,0 +1,68 @@
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const cubeGeometry = new THREE.BoxGeometry(15, 15, 15);
+
+const colorRed = new THREE.Color(0xff0000);
+const colorGreen = new THREE.Color(0x00ff00);
+const colorBlue = new THREE.Color(0x0000ff);
+const colorYellow = new THREE.Color(0xffff00);
+const colorMagenta = new THREE.Color(0xff00ff);
+const colorCyan = new THREE.Color(0x00ffff);
+
+const cubeMaterials = [
+  new THREE.MeshPhongMaterial({ color: colorRed, shininess: 100, specular: 0x111111 }), // Front
+  new THREE.MeshPhongMaterial({ color: colorGreen, shininess: 100, specular: 0x111111 }), // Back
+  new THREE.MeshPhongMaterial({ color: colorBlue, shininess: 100, specular: 0x111111 }), // Top
+  new THREE.MeshPhongMaterial({ color: colorYellow, shininess: 100, specular: 0x111111 }), // Bottom
+  new THREE.MeshPhongMaterial({ color: colorMagenta, shininess: 100, specular: 0x111111 }), // Left
+  new THREE.MeshPhongMaterial({ color: colorCyan, shininess: 100, specular: 0x111111 }), // Right
+];
+
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
+cube.position.set(0, 0, -50); // set cube position
+
+scene.add(cube);
+
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(20, 20, 20);
+
+scene.background = new THREE.Color(0x404040);
+scene.add(light);
+scene.add(new THREE.AmbientLight(0x404040));
+
+camera.position.z = 1;
+camera.position.y = 1;
+camera.position.x = 1;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+animate();
+
+// Add click event listener to the cube
+const canvas = document.getElementsByTagName('canvas')[0];
+canvas.addEventListener('click', () => {
+  window.location.href = 'index.html';
+});
+
+// Resize window
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
